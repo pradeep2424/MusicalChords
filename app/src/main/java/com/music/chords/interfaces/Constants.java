5d2508f7bf883b23e456155c6fbfd180a35241fc
pradeep2424 <pradeepjadhav18@gmail.com>
2021-05-28 09:34:40 +0530

    added ROOT_URL

--- app/src/main/java/com/music/chords/interfaces/Constants.java
+++ app/src/main/java/com/music/chords/interfaces/Constants.java
@@ -1,17 +1,8 @@
 package com.music.chords.interfaces;
 
 public interface Constants {
-    public static final String ROOT_URL = "http://api.androidhive.info/json/";
-
-    //    String BASE_URL = "http://svck12intprod.sevenpv.com/CustomerAppService1.svc/";
-//    String BASE_URL = "http://svck12qaprod.sevenpv.com/CustomerAppService1.svc/";
-    String BASE_URL = "http://apibeta.k12insight.com/CustomerAppService1.svc/";
-
-    //String BASE_URL = "http://172.16.7.43/UI/CustomerAppService1.svc/";
-    //    String BASE_URL = "http://172.16.7.17:98/CustomerAppService1.svc/"; //shoeb
-    // String BASE_URL = "http://localhost/UI//CustomerAppService1.svc/";
-
-//    String BASE_URL = "http://172.16.7.43:86/CustomerAppService1.svc/";    // chandan thakur
+//    public static final String ROOT_URL = "http://api.androidhive.info/json/";
+    public static final String ROOT_URL = "http://api.christianresources.online/RehobotGeetService.svc/";
 
     String SONG_OBJECT = "SongObject";
     String APP_SHARED_PREFERENCE = "SPMusicChords";
